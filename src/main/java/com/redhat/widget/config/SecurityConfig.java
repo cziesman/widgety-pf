995d05cbfc4a20391bb6cf6133eda42baef5dccc
Craig Ziesman <cziesman@redhat.com>
2025-01-13 14:40:30 -0700

    working on oauth2

--- src/main/java/com/redhat/widget/config/SecurityConfig.java
+++ src/main/java/com/redhat/widget/config/SecurityConfig.java
@@ -1,38 +1,54 @@
 package com.redhat.widget.config;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.authority.mapping.GrantedAuthoritiesMapper;
+import org.springframework.security.oauth2.core.oidc.OidcIdToken;
+import org.springframework.security.oauth2.core.oidc.user.OidcUserAuthority;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 
-import static org.springframework.security.config.Customizer.withDefaults;
-
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity(securedEnabled = true, jsr250Enabled = true)
 @EnableConfigurationProperties(ApplicationUsers.class)
+@Slf4j
 public class SecurityConfig {
+
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http, MvcRequestMatcher.Builder mvc) throws Exception {
 
         http
-                .csrf(AbstractHttpConfigurer::disable);
+                .csrf(Customizer.withDefaults());
         http
-                .cors(AbstractHttpConfigurer::disable);
+                .cors(Customizer.withDefaults());
         http
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(new AntPathRequestMatcher("/favicon.ico"))
                         .permitAll()
                         .requestMatchers(new AntPathRequestMatcher("/actuator/**"))
+                        .permitAll()
+                        .requestMatchers(new AntPathRequestMatcher("/login/**"))
+                        .permitAll()
+                        .requestMatchers(new AntPathRequestMatcher("/logout/**"))
+                        .permitAll()
+                        .requestMatchers(new AntPathRequestMatcher("/oauth2/**"))
                         .permitAll()
                         .requestMatchers(new AntPathRequestMatcher("/resources/**"))
                         .permitAll()
@@ -41,17 +57,29 @@ public class SecurityConfig {
                         .anyRequest()
                         .authenticated());
         http
-                .oauth2Login(withDefaults());
+//                .oauth2Login(withDefaults());
+                .oauth2Login(oauth2 -> oauth2
+                        .userInfoEndpoint(userInfo -> userInfo.userAuthoritiesMapper(userAuthoritiesMapper())));
+        http
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED));
 
         //        http
 //                .formLogin((formLogin) -> formLogin.loginPage("/login.faces")
 //                        .failureUrl("/login.faces?error=true")
 //                        .defaultSuccessUrl("/widgets.faces")
 //                        .permitAll());
+//        http
+//                .logout((logout) -> logout
+//                        .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+//                        .clearAuthentication(true)
+//                        .invalidateHttpSession(true)
+//                        .logoutSuccessUrl("/widgets.faces")
+//                        .deleteCookies("JSESSIONID"));
+
         http
-                .logout((logout) -> logout
-                        .logoutSuccessUrl("/widgets.faces")
-                        .deleteCookies("JSESSIONID"));
+                .oidcLogout((logout) -> logout
+                        .backChannel(Customizer.withDefaults()));
 
         return http.build();
     }
@@ -61,6 +89,31 @@ public class SecurityConfig {
     MvcRequestMatcher.Builder mvc(HandlerMappingIntrospector introspector) {
 
         return new MvcRequestMatcher.Builder(introspector);
+    }
+
+
+    private GrantedAuthoritiesMapper userAuthoritiesMapper() {
+
+        // this is a complete hack because I cannot figure out how to get keycloak to return the custom client roles.
+        return (authorities) -> {
+            Set<GrantedAuthority> mappedAuthorities = new HashSet<>();
+
+            authorities.forEach(authority -> {
+                if (authority instanceof OidcUserAuthority oidcUserAuthority) {
+
+                    OidcIdToken idToken = oidcUserAuthority.getIdToken();
+
+                    if ("jane".equals(idToken.getPreferredUsername())) {
+                        SimpleGrantedAuthority simpleGrantedAuthority = new SimpleGrantedAuthority("ROLE_ADMIN");
+                        mappedAuthorities.add(simpleGrantedAuthority);
+                    }
+                    SimpleGrantedAuthority simpleGrantedAuthority = new SimpleGrantedAuthority("ROLE_USER");
+                    mappedAuthorities.add(simpleGrantedAuthority);
+                }
+            });
+
+            return mappedAuthorities;
+        };
     }
 
 //    /**
@@ -83,5 +136,4 @@ public class SecurityConfig {
 //        }
 //        return result;
 //    }
-
 }
--- src/main/java/com/redhat/widget/ui/DefaultWebController.java
+++ src/main/java/com/redhat/widget/ui/DefaultWebController.java
@@ -1,11 +1,15 @@
 package com.redhat.widget.ui;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping
+@Slf4j
 public class DefaultWebController {
 
     @GetMapping({
@@ -13,7 +17,18 @@ public class DefaultWebController {
     })
     public String index() {
 
-        return"redirect:/login.faces";
-}
+        return "redirect:/widgets.faces";
+    }
+
+    @GetMapping("/dologout")
+    public String doLogout() {
+
+        SecurityContextHolder.clearContext();
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        LOG.debug("{}", authentication);
+        SecurityContextHolder.clearContext();
+
+        return "redirect:/logout";
+    }
 
 }
